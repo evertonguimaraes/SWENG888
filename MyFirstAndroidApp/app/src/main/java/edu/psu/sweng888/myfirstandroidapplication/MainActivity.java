3395b6fa3653bc3049feee7677a5574bfe9fb1d3
evertonguimaraes <ezt157@psu.edu>
2023-03-21 17:25:10 -0400

    Updating constants

--- MyFirstAndroidApp/app/src/main/java/edu/psu/sweng888/myfirstandroidapplication/MainActivity.java
+++ MyFirstAndroidApp/app/src/main/java/edu/psu/sweng888/myfirstandroidapplication/MainActivity.java
@@ -7,57 +7,56 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String currentActivityState = "Current State: ";
-    private String methodDescription = "Called When:  ";
+    private final String CURRENT_STATE = "Current State: ";
+    private final String METHOD_CALLED = "Called When:  ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(currentActivityState, "onCreate()");
-        Log.d(methodDescription, "the activity is first created.");
+        Log.d(CURRENT_STATE, "onCreate()");
+        Log.d(METHOD_CALLED, "the activity is first created.");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(currentActivityState, "onStart()");
-        Log.d(methodDescription, "the activity is about to become visible.");
+        Log.d(CURRENT_STATE, "onStart()");
+        Log.d(METHOD_CALLED, "the activity is about to become visible.");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(currentActivityState, "onResume()");
-        Log.d(methodDescription, "activity will start interacting with the user.");
+        Log.d(CURRENT_STATE, "onResume()");
+        Log.d(METHOD_CALLED, "activity will be ready to start interacting with the user.");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(currentActivityState, "onPause()");
-        Log.d(methodDescription, "the activity is taking focus");
+        Log.d(CURRENT_STATE, "onPause()");
+        Log.d(METHOD_CALLED, "the activity is taking focus");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(currentActivityState, "onStop()");
-        Log.d(methodDescription, "the activity is no longer visible.");
+        Log.d(CURRENT_STATE, "onStop()");
+        Log.d(METHOD_CALLED, "the activity is no longer visible.");
 
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(currentActivityState, "onDestroy()");
-        Log.d(methodDescription, "the activity is destroyed");
+        Log.d(CURRENT_STATE, "onDestroy()");
+        Log.d(METHOD_CALLED, "the activity is destroyed");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(currentActivityState, "onRestart()");
-        Log.d(methodDescription, "the activity stopped prior to start.");
-
+        Log.d(CURRENT_STATE, "onRestart()");
+        Log.d(METHOD_CALLED, "the activity stopped prior to start.");
     }
 }

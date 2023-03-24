5d05190e09b9fe69c5578d2a8195698a8a3b8f55
evertonguimaraes <ezt157@psu.edu>
2023-03-24 19:26:42 -0400

    Create project for Lesson 3
    - Create the UI

---
+++ LessonThree_Intents/app/src/main/java/edu/psu/sweng888/lessonthree_intents/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.psu.sweng888.lessonthree_intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    
+}

ec4393dc91fc7ad27a9b25d417a89b898b82996c
evertonguimaraes <ezt157@psu.edu>
2023-03-24 19:27:46 -0400

    Wire UI to the MainActivity

--- LessonThree_Intents/app/src/main/java/edu/psu/sweng888/lessonthree_intents/MainActivity.java
+++ LessonThree_Intents/app/src/main/java/edu/psu/sweng888/lessonthree_intents/MainActivity.java
@@ -11,11 +11,28 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView mImageView;
+    private Button mButtonMakeCall;
+    private Button mButtonSendEmail;
+    private Button mButtonSendSMS;
+    private Button mButtonCreateNote;
+    private Button mButtonListCourses;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mImageView = (ImageView) findViewById(R.id.imageViewAvatar);
+        mImageView.setImageResource(R.mipmap.ic_alien);
+
+        /** buttons that will be associated with the use of Implicit Intents */
+        mButtonMakeCall = findViewById(R.id.buttonMakeCall);
+        mButtonSendEmail = findViewById(R.id.buttonSendEmail);
+        mButtonSendSMS = findViewById(R.id.buttonSendSMS);
+        mButtonCreateNote = findViewById(R.id.buttonCreateNote);
+
+        /** Button that will be associated with the use of Explicit Intent */
+        mButtonListCourses = findViewById(R.id.buttonCheckListCourses);
     }
-    
 }

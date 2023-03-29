0a8de856a2cc9fe4722e474d3014ddb82d31c8ed
evertonguimaraes <ezt157@psu.edu>
2023-03-29 00:38:04 -0400

    Updating project
    1) Included all the examples with implicit intents.
    2) Included the ListView, ListAdapter and Course (implementing Serializable or Parcelable.
    3) Included Explicit Intents
    4) Included Example Passing and Recovering Parameters.

--- LessonThree_Intents/app/src/main/java/edu/psu/sweng888/lessonthree_intents/activity/MainActivity.java
+++ LessonThree_Intents/app/src/main/java/edu/psu/sweng888/lessonthree_intents/activity/MainActivity.java
@@ -1,8 +1,13 @@
 package edu.psu.sweng888.lessonthree_intents.activity;
 
+import static android.Manifest.permission.CALL_PHONE;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
 import android.view.View;
@@ -16,7 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView mImageView;
     private Button mButtonOpenDialer;
     private Button mButtonSendEmail;
-    private Button mButtonSendSMS;
+    private Button mButtonSetAlarm;
     private Button mButtonCreateNote;
     private Button mButtonListCourses;
     private Button mButtonOpenFile;
@@ -31,32 +36,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mButtonOpenDialer = findViewById(R.id.buttonMakeCall);
         mButtonSendEmail = findViewById(R.id.buttonSendEmail);
-        mButtonSendSMS = findViewById(R.id.buttonSetAlarm);
+        mButtonSetAlarm = findViewById(R.id.buttonSetAlarm);
         mButtonCreateNote = findViewById(R.id.buttonCreateNote);
         mButtonOpenFile = findViewById(R.id.buttonOpenFile);
-
-        mButtonOpenDialer.setOnClickListener(this);
-        mButtonOpenDialer.setOnLongClickListener(this);
-
         mButtonListCourses = findViewById(R.id.buttonCheckListCourses);
 
+        /** Defining the OnCLickListener for all the Buttons */
+        mButtonOpenDialer.setOnLongClickListener(this);
         mButtonSendEmail.setOnClickListener(this);
-        mButtonSendSMS.setOnClickListener(this);
+        mButtonSetAlarm.setOnClickListener(this);
         mButtonCreateNote.setOnClickListener(this);
         mButtonListCourses.setOnClickListener(this);
         mButtonOpenFile.setOnClickListener(this);
+
+        /** Set the OnLongCLickListener for the OpenDialer Button */
+        mButtonOpenDialer.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
 
         switch (v.getId()){
-            case R.id.buttonMakeCall: openDialer(); break;
-            case R.id.buttonSendEmail: sendEmail(); break;
-            case R.id.buttonSetAlarm: setAlarm();break;
+            case R.id.buttonMakeCall: openDialer(); break; // DONE
+            case R.id.buttonSendEmail: sendEmail(); break; // DONE
+            case R.id.buttonSetAlarm: setAlarm();break; // DONE
             case R.id.buttonCreateNote: openGoogleKeepNotes(); break;
-            case R.id.buttonOpenFile: openPDFFile(); break;
-            case R.id.buttonCheckListCourses: showListCourses(); break;
+            case R.id.buttonCheckListCourses: showListCourses(); break; // DONE
             default: break;
         }
     }
@@ -68,30 +74,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void makeCall(){
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        String phoneNumber = "6106483224";
+        intent.setData(Uri.parse("tel:" + phoneNumber));
 
+        /** As a good practice, we need to check if the permission was granted by the user
+         * If the permission is granted, it will start the activity;
+         * Otherwise, it will request the permission to perform the CALL_PHONE action */
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE) ==
+                PackageManager.PERMISSION_GRANTED) {
+            startActivity(intent);
+        } else
+            requestPermissions(new String[]{CALL_PHONE}, 1);
+
+        /** It will check if the Package Manager has an active instance
+         * THe Package Managed helps in solving any dependencies with classes, services, etc.
+         * It is also able to install or uninstall any required package depending on the component used */
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     private void openDialer(){
-
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        if (intent.resolveActivity(getPackageManager() )!= null){
+            startActivity(intent);
+        }
     }
 
     private void sendEmail(){
+        String subject = "Mobile Computing";
+        String message = "Composing a new email";
+        String email = "evertontguimaraes@gmail.com";
 
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, email);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     private void setAlarm(){
-
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+        intent.putExtra(AlarmClock.EXTRA_HOUR, 19);
+        intent.putExtra(AlarmClock.EXTRA_MINUTES, 7);
+        intent.putExtra(AlarmClock.EXTRA_MESSAGE, "Class Break");
+        startActivity(intent);
     }
 
     private void openGoogleKeepNotes(){
-
-    }
-
-    private void openPDFFile(){
-
+        // TODO Implement this version in the next class.
     }
 
     private void showListCourses(){
-
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(intent);
     }
 }
--- LessonThree_Intents/app/src/main/java/edu/psu/sweng888/lessonthree_intents/model/CourseP.java
+++ LessonThree_Intents/app/src/main/java/edu/psu/sweng888/lessonthree_intents/model/CourseP.java
@@ -1,5 +1,42 @@
 package edu.psu.sweng888.lessonthree_intents.model;
 
-public class CourseP {
+import android.os.Parcel;
+import android.os.Parcelable;
 
+public class CourseP implements Parcelable {
+
+    private int ID;
+    private int credits;
+    private String description;
+
+
+    protected CourseP(Parcel in) {
+        ID = in.readInt();
+        credits = in.readInt();
+        description = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(ID);
+        dest.writeInt(credits);
+        dest.writeString(description);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<CourseP> CREATOR = new Creator<CourseP>() {
+        @Override
+        public CourseP createFromParcel(Parcel in) {
+            return new CourseP(in);
+        }
+
+        @Override
+        public CourseP[] newArray(int size) {
+            return new CourseP[size];
+        }
+    };
 }

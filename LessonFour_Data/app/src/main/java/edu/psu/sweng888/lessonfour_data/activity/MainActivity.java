e1f48cdf3b54554062b714d643b7226e493a8531
evertonguimaraes <ezt157@psu.edu>
2023-04-06 15:00:33 -0400

    Project Updates
    - Included a preview page where the user can select to see the full list of movies, or select a determined category.
    - Both options are linked to the SQLite database.
    - in the Main Activity, there is back button and floating button. The back button will return to the selection activity, while the floating button will open an AlertDialog and allow the user to select YES or NO, in case a new movie will be added.
    
    For the upcoming update, the plan is to implement the AddMovieActivity to insert a new movie into the database. In addition, it will return to the previous activity and update the RecyclerView with the new movie.

--- LessonFour_Data/app/src/main/java/edu/psu/sweng888/lessonfour_data/activity/MainActivity.java
+++ LessonFour_Data/app/src/main/java/edu/psu/sweng888/lessonfour_data/activity/MainActivity.java
@@ -1,10 +1,18 @@
 package edu.psu.sweng888.lessonfour_data.activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -14,12 +22,14 @@ import edu.psu.sweng888.lessonfour_data.model.Movie;
 import edu.psu.sweng888.lessonfour_data.model.MovieAdapter;
 import edu.psu.sweng888.lessonfour_data.model.SpacingItemDecorator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView recyclerView;
     private LinearLayoutManager layoutManager;
     private MovieDatabaseHelper databaseHelper;
     private MovieAdapter movieAdapter;
+    private Button mButtonBackSelection;
+    private FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +37,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recycler_view);
+        mFloatingActionButton = findViewById(R.id.floating_action_button);
+        mButtonBackSelection = findViewById(R.id.button_back_to_selection_activity);
+
+        mFloatingActionButton.setOnClickListener(this);
+        mButtonBackSelection.setOnClickListener(this);
+
         databaseHelper = new MovieDatabaseHelper(this);
 
-        this.deleteDatabase("movie_database");
-        List<Movie> movies = databaseHelper.getAllMovies();
-        if(movies.isEmpty()){
+        List<Movie> movies;
+        Intent intent = getIntent();
+        String option_selected = (String) intent.getStringExtra("option_selected");
+
+        if(databaseHelper.isDatabaseEmpty()){
             databaseHelper.populateMoviesDatabase();
             movies = databaseHelper.getAllMovies();
+        }
+
+        if (option_selected.equals("all")){
+            movies = databaseHelper.getAllMovies();
+        } else {
+            movies = databaseHelper.getMoviesByCategory(option_selected.toLowerCase());
         }
 
         movieAdapter = new MovieAdapter(movies);
@@ -42,6 +66,40 @@ public class MainActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this,
                 LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
-
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.floating_action_button: showDialog(); break;
+            case R.id.button_back_to_selection_activity: goBackToPreviousActivity(); break;
+            default: break;
+        }
+    }
+
+    private void goBackToPreviousActivity(){
+        Intent intent = new Intent(MainActivity.this, SelectionActivity.class);
+        startActivity(intent);
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Movie");
+        builder.setMessage("Do you want to add a new movie?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                startActivity(new Intent(MainActivity.this, AddMovieActivity.class));
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "The action was cancelled", Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }

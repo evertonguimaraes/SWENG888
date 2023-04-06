c2bd85daa358f5468efcf3eed232f56aca905a6d
evertonguimaraes <ezt157@psu.edu>
2023-04-06 17:41:06 -0400

    Documenting the source code (MainActivity.java)

--- LessonFour_ContentProvider/app/src/main/java/edu/psu/sweng888/lessonfour_contentprovider/MainActivity.java
+++ LessonFour_ContentProvider/app/src/main/java/edu/psu/sweng888/lessonfour_contentprovider/MainActivity.java
@@ -52,13 +52,23 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("Range")
     private ArrayList<Contact> getContacts() {
+        /** Create an ArrayList of Contact objects */
         ArrayList<Contact> contacts = new ArrayList<>();
+        /** Create a ContentResolver object. It will be responsible for querying
+         * the contacts database (native app) **/
         ContentResolver contentResolver = getContentResolver();
+        /** The ContentResolver will query the ContactsContract based on a given URI
+         *  to retrieve all contacts. The null values indicate that all colums and contacts
+         *  should be retrieved.
+         */
         Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
 
         if (cursor.moveToFirst()) {
             do {
+                /** We use the Cursor object to iterarate over the results. We will obtain information
+                 *  regarding the ContactID and Name.
+                 */
                 String id = cursor.getString(cursor.getColumnIndex(
                         ContactsContract.Contacts._ID));
                 String name = cursor.getString(cursor.getColumnIndex(
@@ -69,11 +79,16 @@ public class MainActivity extends AppCompatActivity {
                         ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                         ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                         new String[]{id}, null);
-
+                /** We will wuery the ContactsContract.CommonDataKinds.Phone.ID to match the Phone number */
                 if (phoneCursor.moveToFirst()) {
+
                     phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(
                             ContactsContract.CommonDataKinds.Phone.NUMBER));
                 }
+                /** Once it finishes iterating over the results, we close the cursor, and add the name
+                 *  and pgone number to a new instance of Contact. The new Contact object is added to
+                 *  the contacts ArrayList.
+                 */
                 phoneCursor.close();
                 contacts.add(new Contact(name, phoneNumber));
 

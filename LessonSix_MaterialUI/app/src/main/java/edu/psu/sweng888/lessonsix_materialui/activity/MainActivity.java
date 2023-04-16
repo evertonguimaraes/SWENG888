b284d081de0a6225b8f1c1663c8eb832b76320cf
evertonguimaraes <ezt157@psu.edu>
2023-04-16 18:38:54 -0400

    Create Lesson 6 Project
    - Added basic structure
    - Edited the main_activity layout
    - Added the menu layout
    - Added the nav_drawer layout
    - Added Strings
    - Added Colors
    - Created fragments

---
+++ LessonSix_MaterialUI/app/src/main/java/edu/psu/sweng888/lessonsix_materialui/activity/MainActivity.java
@@ -0,0 +1,109 @@
+package edu.psu.sweng888.lessonsix_materialui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
+import edu.psu.sweng888.lessonsix_materialui.R;
+import edu.psu.sweng888.lessonsix_materialui.fragment.HomeFragment;
+import edu.psu.sweng888.lessonsix_materialui.fragment.ProfileFragment;
+import edu.psu.sweng888.lessonsix_materialui.fragment.SettingsFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private Toolbar mToolBar;
+    private BottomNavigationView mBottomNavigationView;
+    private FloatingActionButton mFloatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize views
+        mDrawerLayout = findViewById(R.id.nav_drawer_layout);
+        mNavigationView = findViewById(R.id.nav_view);
+        mToolBar.findViewById(R.id.toolbar);
+        mBottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        mFloatingActionButton = findViewById(R.id.floating_action_button);
+
+        setActionBar(mToolBar);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        replaceFragment(new HomeFragment());
+                        return true;
+                    case R.id.nav_profile:
+                        replaceFragment(new ProfileFragment());
+                        return true;
+                    case R.id.nav_settings:
+                        replaceFragment(new SettingsFragment());
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle floating action button click
+            }
+        });
+
+        // Set default fragment
+        replaceFragment(new HomeFragment());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+//        if (id == R.id.action_settings) {
+//            // Handle settings option click
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
---
+++ LessonSix_MaterialUI/app/src/main/java/edu/psu/sweng888/lessonsix_materialui/fragment/SettingsFragment.java
@@ -0,0 +1,6 @@
+package edu.psu.sweng888.lessonsix_materialui.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+}
